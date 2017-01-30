f4299f185b40b90487bda72a810e466e7ff9b22e
Miguel Gimenez <gimenezgimenezmiguel@gmail.com>
2017-01-30 12:56:38 +0100

    test setup

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const config = require('config');
+const app = express();
+const morgan = require('morgan');
 
+const db=require('./db');
 const router = require('./router.js');
 
-const app = express();
+//don't show the log when it is test
+if(config.util.getEnv('NODE_ENV') !== 'test') {
+    //use morgan to log at command line
+    app.use(morgan('combined')); //'combined' outputs the Apache style LOGs
+}
 
-// app.use(bodyParser);
 app.use(bodyParser.json());
-
 app.use(router);
 
 
 app.listen(8888, function () {
   console.log('Listening on port 8888');
 });
+
+
+module.exports = app; // for testing
---
+++ models/User.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+//book schema definition
+const UserSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    spotifyId: { type: Number, required: true },
+    createdAt: { type: Date, default: Date.now },
+  }
+);
+
+// Sets the createdAt parameter equal to the current time
+UserSchema.pre('save', next => {
+  const now = new Date();
+  if(!this.createdAt) {
+    this.createdAt = now;
+  }
+  next();
+});
+
+//Exports the BookSchema for use elsewhere.
+module.exports = mongoose.model('User', UserSchema);
