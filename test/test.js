a97291ffa5326683930f476c611ca93851db8eb5
Rod Reyes <roderick.reyes@gmail.com>
2017-01-31 18:14:40 +0100

    feat: add test for login process using apiary mock api

--- test/test.js
+++ test/test.js
@@ -1,13 +1,36 @@
 process.env.NODE_ENV = 'test';
+const mongoose = require('mongoose');
+const User = require('../models/User');
+const Buffer = require('buffer/').Buffer;
+const nconf = require('../config/nconf');
 
-var server = require('../index');
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+const server = 'https://private-77bd5f-spotamovie.apiary-mock.com';
+let should = chai.should();
 
-describe('server', function () {
-  before(function () {
-    server.listen(8000);
+chai.use(chaiHttp);
+
+describe('Users', () => {
+  beforeEach((done) => {
+    User.remove({}, (err) => {
+      done();
+    });
   });
 
-  after(function () {
-    server.close();
+  // Test Login
+  describe('/login POST user', () => {
+     it('it should allow user to login using Spotify credentials', (done) => {
+       chai.request(server)
+        .post('/login')
+        .field('code', 'auth_code_3838383838383')
+        .field('redirect_uri','spotamovie://callback')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a('object');
+          res.body.should.have.property('userToken');
+        done();
+      });
+    });
   });
 });
