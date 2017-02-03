72e32e6dd430bcd07740b1ce5ab6ee4af975773c
Miguel Gimenez <gimenezgimenezmiguel@gmail.com>
2017-02-03 12:12:28 +0100

    feat:survey

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -3,6 +3,7 @@ const userController = require('./userController');
 const UserSchema =require('../models/User');
 const raccoon = require('raccoon');
 const config = require('../config/dev.json');
+const _ = require('underscore');
 
 let movieController = {};
 
@@ -74,23 +75,6 @@ movieController.dislike=(req,res)=>{
 };
 
 
-movieController.recommendation=(req,res)=>{
-  if (!req.headers.authorization) return res.sendStatus(400, 'missing authorization header');
-  const token =req.headers.authorization.split(' ')[1];
-  UserSchema.find({userToken:token})
-  .then(response=>{
-    if (response.length>0) {
-      const userId=response[0].spotifyId;
-      let movie;
-      raccoon.recommendFor(userId, 1,rec => {
-
-        return res.send(rec);
-      });
-    }else{
-      return res.sendStatus(401);
-    }
-  });
-};
 movieController.allLikes=(req,res)=>{
   if (!req.headers.authorization) return res.sendStatus(400, 'missing authorization header');
   const token =req.headers.authorization.split(' ')[1];
@@ -98,10 +82,8 @@ movieController.allLikes=(req,res)=>{
   .then(response=>{
     if (response.length>0) {
       const userId=response[0].spotifyId;
-      console.log(userId, 'id');
       let movie;
       raccoon.allLikedFor(userId,(results) => {
-        console.log(results);
         return res.send(results);
       });
     }else{
@@ -127,69 +109,91 @@ movieController.alldislikes=(req,res)=>{
 };
 
 
+movieController.recommendation=(req,res)=>{
+  if (!req.headers.authorization) return res.sendStatus(400, 'missing authorization header');
+  const token =req.headers.authorization.split(' ')[1];
+  UserSchema.find({userToken:token})
+  .then(response=>{
+    if (response.length>0) {
+      const userId=response[0].spotifyId;
+      let movie;
+      raccoon.recommendFor(userId, 10,rec => {
+        return res.send(rec);
+      });
+    }else{
+      return res.sendStatus(401);
+    }
+  });
+};
+
 const findRatedMovies=(userId)=>{
   return new Promise((resolve,reject)=>{
-    let userRated=[];
-    const ratedMovies={};
     raccoon.allWatchedFor(userId,results => {
-      results.forEach(like =>{
-        if (!like.includes('SP'))ratedMovies[like]='rated' ;
-      });
+      const ratedMovies= results.filter(like =>!like.includes('SP'));
       return resolve(ratedMovies);
     });
   });
 };
 
 
+const handleMovies = (moviesToBeSent,n,moviesAllreadyRecommended) =>{
+  moviesToBeSent=_.difference(moviesToBeSent,moviesAllreadyRecommended);
+  console.log(moviesToBeSent, 'moviesToBeSent');
+  console.log('moviesAllreadyRecommended', moviesAllreadyRecommended);
+  return moviesToBeSent.slice(0,n);
+};
+
 
 movieController.survey=(req,res)=>{
 
   let url =`https://api.themoviedb.org/3/discover/movie?api_key=${config.TMDB_API_KEY}`;
-  let numberOfmovies;
-  const returnedMovies=[];
-  let ratedMovies={};
+  let numberOfmovies=1;
+  let k=0;
+  let moviesToBeSent=[];
+  let ratedMovies=[];
   if (!req.headers.authorization) return res.sendStatus(400, 'missing authorization header');
   const token =req.headers.authorization.split(' ')[1];
   UserSchema.find({userToken:token})
   .then(user=>{
-    if (user.length>0) {
+    if (user.length===0) return res.sendStatus(401);
 
-      const userId=user[0].spotifyId;
-      userController.updateUser(userId,{firstLogin:false})
-      .then(user=>{
+    const userId=user[0].spotifyId;
+    userController.updateUser(userId,{firstLogin:true})
+    //Handle first login
+    .then(user=>{
+      if(user.firstLogin){
+        numberOfmovies=4;
+        k=1;
+      }
+      //======================================================
+      // GET TMDB movies
+      //======================================================
+      request.get(url, (error, response, body) => {
+        let receivedMovies=JSON.parse(body).results.map((movie=>movie.id));
 
-        const page=Math.floor(Math.random()*999);
-        url+=`&page=${page}`;
-        request.get(url, (error, response, body) => {
-
-          const tmdbMovies=JSON.parse(body);
-
-          user.firstLogin?numberOfmovies=10: numberOfmovies=3;
-
-          findRatedMovies(userId)
-          .then(response=>{
-            ratedMovies=response;
-            let index=0;
-
-            while (returnedMovies.length<numberOfmovies) {
-              if (!ratedMovies.hasOwnProperty(tmdbMovies.results[index].id)) {
-
-                returnedMovies.push(tmdbMovies.results[index].id);
-              }
-              index++;
-              if (index===tmdbMovies.results.length-1) return movieController.survey(req,res);
-            }
-            return res.send(returnedMovies);
+        findRatedMovies(userId)
+        .then(response=>{
+          ratedMovies=response;
+          moviesToBeSent=moviesToBeSent.concat(handleMovies(receivedMovies,numberOfmovies,ratedMovies));
+          ratedMovies=ratedMovies.concat(moviesToBeSent);
+          //GET POPULAR MOVIES
+          request.get(`${url}&sort_by=popularity.desc`, (error, response, body) => {
+            receivedMovies=JSON.parse(body).results.map((movie=>movie.id));
+            moviesToBeSent=moviesToBeSent.concat(handleMovies(receivedMovies,numberOfmovies,ratedMovies));
+            ratedMovies=ratedMovies.concat(moviesToBeSent);
+            const page =Math.floor(Math.random()*10)+10;
+            url+=`&page=${page}`;
+            //GET WEIRD MOVIES
+            request.get(`${url}&sort_by=popularity.asc`, (error, response, body) => {
+              receivedMovies=JSON.parse(body).results.map((movie=>movie.id));
+              moviesToBeSent=moviesToBeSent.concat(handleMovies(receivedMovies,numberOfmovies,ratedMovies));
+              return res.send(moviesToBeSent);
+            });
           });
         });
       });
-
-    }else{
-
-      return res.sendStatus(401);
-    }
+    });
   });
-
 };
 
 
