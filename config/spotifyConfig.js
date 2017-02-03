9286e1c803c6ca6bd9d3fad657a3ec53152a65c6
Miguel Gimenez <gimenezgimenezmiguel@gmail.com>
2017-02-03 13:04:22 +0100

    chore: restoring config folder

---
+++ config/spotifyConfig.js
@@ -0,0 +1,6 @@
+module.exports = {
+  client_id : 'df38bfb3200c41b7b038828c043f6bfa', // Your client id
+  client_secret : 'b09195611a3f458fba69e75cefd37eff', // Your secret
+  redirect_uri : 'spotamovie://callback', // Your redirect uri
+
+};
